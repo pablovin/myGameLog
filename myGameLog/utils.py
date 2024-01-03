99710d06a42e6926f9c5b81e03ee7c6cc88b0027
Pablo Barros <pablovin@gmail.com>
2024-01-03 21:47:48 +0100

    update utils

--- myGameLog/utils.py
+++ myGameLog/utils.py
@@ -2,7 +2,6 @@ import os
 
 
 def remove_last_empty_row(csv_directory):
-    print(f"Removing empty row from: {csv_directory}")
     with open(csv_directory, "r+") as f:
         f.seek(0, 2)
         size = f.tell()
